28e11ba5174d9441f440f9b66c9234544a638f83
kiavateam <ava@kiava.ir>
2017-07-20 22:59:24 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -17,7 +17,7 @@ local notify = lgi.require('Notify')
 notify.init ("Telegram updates")
 chats = {}
 plugins = {}
-helper_id = 387388679 --Put Your Helper Bot ID Here
+helper_id = 386032084 --Put Your Helper Bot ID Here
 
 function do_notify (user, msg)
 	local n = notify.Notification.new(user, msg)
@@ -88,7 +88,7 @@ function create_config( )
 		"fun",
 
 	},
-    sudo_users = {247134702,387388679,366848429,334984718, sudo_id},
+    sudo_users = {386032084,278259998, sudo_id},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
